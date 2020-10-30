2c8583380d27c0d7c0f00f127d880c353dad769b
Chantal Snazel <clsnazel@gmail.com>
2020-10-29 21:42:27 -0700

    add custom hook to Appt component w/ CREATE, EMPTY + SHOW state

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import useVisualMode from 'hooks/useVisualMode';
 
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
 
 import './styles.scss';
 
-export default function Appointment(props) {
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const ERROR = "ERROR";
+const CREATE = "CREATE";
+const EDIT = "EDIT";
+const SAVING = "SAVING";
 
+
+export default function Appointment(props) {
+  const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  const interviewers = [];
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? 
-        <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : 
-        <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={interviewers} onCancel={back} />}
+
     </article>
   );
 };
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const useVisualMode = function(initMode) {
+  const [mode, setMode] = useState(initMode);
+  const [history, setHistory] = useState([initMode]);
+  
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      history[history.length - 1] = newMode;
+      setMode(newMode);
+    } else {
+      setHistory([
+        ...history,
+        newMode
+      ]);
+      setMode(newMode);
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      const backTo = history[history.length - 1];
+      setMode(backTo);
+    }
+  }
+  return {mode, transition, back}
+}
+
+export default useVisualMode;
